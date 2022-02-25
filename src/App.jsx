0a29d7999a3967cfb4b1e5d44615310b2f0ef0bf
jsk900 <jsk900@yahoo.co.uk>
2022-02-25 13:27:54 +0100

    React useRef form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import { useRef } from 'react';
 import './App.css';
 
 const App = () => {
+  const nameRef = useRef();
+  const ageRef = useRef();
+  const emailRef = useRef();
+  const formDataRef = useRef({ name: '', age: '', email: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    formDataRef.current.name = nameRef.current.value;
+    formDataRef.current.age = ageRef.current.value;
+    formDataRef.current.email = emailRef.current.value;
+
+    nameRef.current.value = '';
+    ageRef.current.value = '';
+    emailRef.current.value = '';
+  };
+
+  console.log(formDataRef.current);
+
   return (
     <main>
-     <h1>Welcome to React</h1>
+      <h1>Welcome to React</h1>
+      <form>
+        <input
+          type='text'
+          placeholder='Name'
+          //value={nameRef.current}
+          ref={nameRef}
+        />
+        <input
+          type='text'
+          placeholder='Age'
+          //value={ageRef.current}
+          ref={ageRef}
+        />
+        <input
+          type='text'
+          placeholder='Email'
+          //value={emailRef.current}
+          ref={emailRef}
+        />
+        <button onClick={handleSubmit}>submit</button>
+      </form>
+      <p>{formDataRef.current.name}</p>
+      <p>{formDataRef.current.age}</p>
+      <p>{formDataRef.current.email}</p>
     </main>
   );
-}
+};
 
 export default App;
-
